b026b1cff54a0ffb3f70644d710ccef572d541b9
senthil100695 <senthil100695@gmail.com>
2023-11-20 17:21:32 -1200

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'senthil100695',
+    author_email= 'senthil100695@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
